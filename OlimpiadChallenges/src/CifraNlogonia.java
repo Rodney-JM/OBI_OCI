999a5b30c88524c1b1a9c7efe1d1b6aacb079e11
Rodney <rondineypatricio@gmail.com>
2024-04-06 12:43:06 -0300

    Fazer desafio da cifra de Ngolonia

---
+++ OlimpiadChallenges/src/CifraNlogonia.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class CifraNlogonia {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        System.out.println(descobrirCifra(input));
+        scan.close();
+    }
+
+    public static String descobrirCifra(String palavra){
+        StringBuilder cifra = new StringBuilder();
+        String alfabeto = "abcdefghijklmnopqrstuvwxyz";
+        String vogais = "aeiou";
+
+        for (char c: palavra.toCharArray()){
+            if (isConsoante(c)){
+                cifra.append(c);
+                cifra.append(getVogalMaisProxima(c));
+            }
+        }
+    }
+
+    public static boolean isConsoante(char c){
+        String consoantes = "bcdfghjklmnpqrstvwxyz";
+        return consoantes.indexOf(c) != -1;
+    }
+
+    public static char getVogalMaisProxima(char c){
+        String vogais = "aeiou";
+        int index = vogais.indexOf(c);
+        if (index == -1){
+            int anterior = 0;
+            int proxima = 0;
+            for (int i=0; i<vogais.length(); i++){
+                if (vogais.charAt(i)< c){
+                    anterior = vogais.charAt(i);
+                }else{
+                    proxima = vogais.charAt(i);
+                    break;
+                }
+            }
+        }
+    }
+}
